da41e3cd72a79fac8b7c83b6bf62950c6982cdd8
AlexanderTalan <talan_ae@mail.ru>
2022-12-15 22:08:54 +0300

    Create HomeWork folder

---
+++ Practicum1/КвадратЧисла.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        //int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число: ");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Квадрат {a} = {a * a}");
+    }
+}
+
