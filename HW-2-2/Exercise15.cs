e67dd529f3c0f7dcab521321b6e468568f299421
AlexanderTalan <talan_ae@mail.ru>
2022-12-20 00:29:15 +0300

    create HW-2

---
+++ HW-2-2/Exercise15.cs
@@ -0,0 +1,74 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+while(true)
+{
+    Console.Clear();
+
+    int value;
+    try
+    {
+        System.Console.WriteLine("Введите номер дня: ");
+        value = int.Parse(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        System.Console.WriteLine("Постарайтесь ввести ТОЛЬКО цифры!)");
+        Console.ReadLine();
+        continue;
+    }
+
+    if (value <= 0)
+    {
+        System.Console.WriteLine("Это не подходит :)\nнажмите Enter");
+        Console.ReadLine();
+        continue;
+    }
+
+    System.Console.WriteLine(weekEnd(normalizeValue(value)));
+
+    Console.ReadLine();
+    break;
+}
+
+static int normalizeValue (int Day)
+{
+    int numberOfDay;
+    if (Day % 7 == 0)
+    {
+        numberOfDay = 7;
+    }
+    else
+    {
+        if (Day > 7)
+        {
+            numberOfDay = Day - (Day / 7) * 7;
+        }
+        else
+        {
+            numberOfDay = Day;
+        }
+    }
+
+    return numberOfDay;
+}
+
+static string weekEnd (int numer)
+{
+    string weekEnd;
+
+    if (numer == 6 || numer == 7)
+    {
+        weekEnd = $"День выходной, {numer}-й";
+
+    }
+    else
+    {
+        weekEnd = $"Рабочий день, номер - {numer}";
+    }
+
+    return weekEnd;
+}
--- HW-2/HW-2.cs
+++ HW-2/HW-2.cs
@@ -35,30 +35,3 @@ else
 
 System.Console.WriteLine("Для продолжения нажмите Enter ...");  // Переходим к следующей задаче
 Console.ReadLine();                                             // Очищаем экран
-
-// Задача 15: Напишите программу, которая принимает на вход цифру, 
-//обозначающую день недели, и проверяет, является ли этот день выходным.
-
-Console.Clear();
-
-Console.Write("Введите число: ");
-
-int numberOfDay= int.Parse(Console.ReadLine());
-
-
-if (numberOfDay == 6 || numberOfDay == 7)
-{
-    System.Console.WriteLine("Введённый номер дня - выходной");
-} 
-else if (numberOfDay <0 || numberOfDay == 0 || numberOfDay > 7)
-{
-    System.Console.WriteLine("Введено неверное число!");   
-}
-else
-{
-    System.Console.WriteLine("Введённый номер дня - будний день");
-}
-
-
-System.Console.WriteLine("Для продолжения нажмите Enter ...");  // Переходим к следующей задаче
-Console.ReadLine();                                             // Очищаем экран
