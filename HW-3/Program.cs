3a8f451f3f9063cb4018dfa84fc6119dfb7c0741
AlexanderTalan <talan_ae@mail.ru>
2022-12-24 23:16:02 +0300

    Create PolyNumbers File

--- HW-3/Program.cs
+++ HW-3/Program.cs
@@ -1,1 +1,26 @@
 ﻿// HomeWork 3
+// Задача 19
+
+/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.Clear();
+
+for (int i = 10000; i <= 99999; i++)
+{
+    FindPolyNumbers(i);
+}
+
+static void FindPolyNumbers (int num)
+{
+    string anyNumber = Convert.ToString(num);
+    if ((anyNumber.Substring(0, 1) == anyNumber.Substring(4, 1)) && (anyNumber.Substring(1, 1) == anyNumber.Substring(3, 1)))
+    {
+        Console.WriteLine("Число {0} - полиндром", num);
+    }
+
+}
